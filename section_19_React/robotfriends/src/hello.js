4c9caa668ab88a177bb7d05336ee1cc809b48c13
ridvanms <ridvans@abv.bg>
2023-01-08 01:25:34 +0200

    updating

---
+++ section_19_React/robotfriends/src/hello.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import './hello.css'
+class Hello extends Component {
+    render() {
+        return (
+            <h1 className="f1 tc">Hello</h1>
+        )
+    }
+}
+export default Hello
