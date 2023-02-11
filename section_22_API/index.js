a13424f9ff1566f6c01f8a9ecbe17253651c877d
ridvanms <ridvans@abv.bg>
2023-02-11 11:16:44 +0200

    updating

--- section_22_API/index.js
+++ section_22_API/index.js
@@ -8,3 +8,21 @@ async function takeAPI(url){
     }
 }
 takeAPI('https://swapi.py4e.com/api/people')
+
+// const axios = require("axios");
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://bloomberg-market-and-financial-news.p.rapidapi.com/market/auto-complete',
+//   params: {query: '<REQUIRED>'},
+//   headers: {
+//     'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+//     'X-RapidAPI-Host': 'bloomberg-market-and-financial-news.p.rapidapi.com'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
