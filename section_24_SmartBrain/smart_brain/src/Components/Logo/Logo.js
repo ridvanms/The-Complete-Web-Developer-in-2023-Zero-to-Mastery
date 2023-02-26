1d74f59ea72ee312d97edb78071f1d697e89940c
Ridvan <ridvans@abv.bg>
2023-02-26 19:30:35 +0200

    updating

--- section_24_SmartBrain/smart_brain/src/Components/Logo/Logo.js
+++ section_24_SmartBrain/smart_brain/src/Components/Logo/Logo.js
@@ -1,23 +1,19 @@
-import React from "react";
-
+import React from 'react';
+// import Tilt from 'react-tilt'; 
 import Tilt from 'react-parallax-tilt';
-import './logo.css'
+import brain from './brain.png';
+import './Logo.css';
 
-const Logo = ()=>{
-    return (
-        <div className="ma4 mt0">
-            <Tilt className="Tilt br2 shadow-3 " option={{max:70}} style={{ height: '10em',width:'10em',display: 'flex',justifyContent: 'flex-start' }}> 
-                <div >
-                    <img  style={{height:'5em',width:'30em',paddingTop:'2.5em' }}  src= "https://cdn.pixabay.com/photo/2022/12/08/06/39/artificial-7642628__340.png" alt="SmartBrain"/>
-
-                </div>
-            </Tilt>
+const Logo = () => {
+  return (
+    <div className='ma4 mt0'>
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+        <div className="Tilt-inner pa3">
+          <img style={{paddingTop: '5px'}} alt='logo' src={brain}/>
         </div>
-        
-       
-    )
+      </Tilt>
+    </div>
+  );
 }
+
 export default Logo;
-// Tilt br2 shadow-2 
-// Tilt-inner pa3 
-// style={{height:'123px',width:'30em', marginBottom:'3em' }}
--- section_24_SmartBrain/smart_brain/src/Components/Rank/Rank.js
+++ section_24_SmartBrain/smart_brain/src/Components/Rank/Rank.js
@@ -1,18 +1,16 @@
-import React from "react";
-import './Rank.css'
+import React from 'react';
 
-const Rank = () => {
-    const name = "Maho"
-    let rank = 0
-    return (
-        <div>
-            <div className={`white f3`}>
-                {`${name} your rank is `}
-            </div>
-            <div className={`white f1`}>
-                {`#${rank}`}
-            </div>
-        </div>
-    )
+const Rank = ({ name, entries }) => {
+  return (
+    <div>
+      <div className='white f3'>
+        {`${name}, your current entry count is...`}
+      </div>
+      <div className='white f1'>
+        {entries}
+      </div>
+    </div>
+  );
 }
+
 export default Rank;
