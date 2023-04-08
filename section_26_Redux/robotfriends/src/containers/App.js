8a2147d1e53d731caa7ec995f1e37ffffe0ddb6f
ridvanms <ridvans@abv.bg>
2023-04-08 10:48:48 +0300

    using redux

---
+++ section_26_Redux/robotfriends/src/containers/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import CardList from "../components/cardList";
+import SearchBox from "../components/searchBox";
+import Scroll from "../components/scroll";
+import "../assets/style/app.css";
+
+import { setSearchField, requestRobots } from "../action";
+
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots()),
+  };
+};
+
+class App extends Component {
+  //   constructor() {
+  //     super();
+  //     this.state = {
+  //       robots: [],
+  //     };
+  //   }
+  componentDidMount() {
+    this.props.onRequestRobots();
+  }
+
+  render() {
+    const { searchField, onSearchChange, robots, isPending } = this.props;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+    return isPending ? (
+      <h1>Loading</h1>
+    ) : (
+      <div className="tc">
+        <h1>RobotFriends</h1>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ section_26_Redux/robotfriends/src/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createLogger } from "redux-logger";
+
+import thunkMiddleware from "redux-thunk";
+
+import reportWebVitals from "./reportWebVitals";
+import "tachyons";
+
+import "./assets/style/index.css";
+import App from "./containers/App";
+import { searchRobots, requestRobots } from "./reducers.js";
+
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger)
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
